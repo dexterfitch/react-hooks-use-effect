3ace2ec84a388464d73d254b680d804356cda096
Dex Fitch <fitch.dexter@gmail.com>
2021-09-26 15:08:26 -0400

    Solved + Discussion

--- src/DogPics.js
+++ src/DogPics.js
@@ -4,24 +4,27 @@ import React, { useState, useEffect } from "react";
 // to fix, pass an empty array as the second argument for useEffect
 function DogPics() {
   const [images, setImages] = useState([]);
+  const [randomClick, setRandomClick] = useState(false);
 
   useEffect(() => {
-    console.log("useEffect");
+    console.log("useEffect fetch is being called...")
     fetch("https://dog.ceo/api/breeds/image/random/3")
       .then((r) => r.json())
       .then((data) => {
-        console.log("setState");
         setImages(data.message);
       });
-  });
+  }, [randomClick]);
 
-  console.log("render");
+  function randomizerClick() {
+    setRandomClick(randomClick ? false : true)
+  }
 
   return (
     <div>
       {images.map((image) => (
-        <img src={image} key={image} />
+        <img src={image} key={image} alt={image} />
       ))}
+      <button onClick={randomizerClick}>Give me 3 More Random Dogs!!!!</button>
     </div>
   );
 }
